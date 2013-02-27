dc9033cba682c98eb4da263545a1802867e22878
Arseniy Krasnov <arseniy@krasnoff.org>
2013-02-28 03:01:44 +0400

    Add make archive

--- plugin.py
+++ plugin.py
@@ -6,7 +6,7 @@ class Archiver():
 	def __init__(self, main_window)	:
 		self.main_window = main_window
 		self.extract_mimes = ('application/zip','application/x-tar', 'application/x-compressed-tar')
-		self.create_mimes = ('application/octet-stream')
+		self.create_mimes = ('application/octet-stream','inode/directory','text/directory','folder')
 		self.extract_menu = (
 		{
 		'label': 'Archiver',
@@ -60,7 +60,19 @@ class Archiver():
 			print e
 
 	def make_archive(self, widget, data):
-		pass
+		filepath = self.get_selection()
+		print filepath
+		zfile = filepath.rstrip('/') + '.zip'
+		filepath = filepath.rstrip('/')
+		zf = zipfile.ZipFile(zfile, mode='w')
+		if os.path.isfile(filepath):
+			zf.write(filepath, filepath[len(os.path.dirname(filepath)):].strip('/'), compress_type=zipfile.ZIP_DEFLATED)
+		else:
+			for root, dirs, files in os.walk(filepath):
+				for name in files:
+					file_to_zip = os.path.join(root, name)
+					arcname = file_to_zip[len(os.path.dirname(filepath)):].strip('/')
+					zf.write(file_to_zip, arcname, compress_type=zipfile.ZIP_DEFLATED)
 
 
 def register_plugin(application):
@@ -69,4 +81,6 @@ def register_plugin(application):
 
 	for item in archiver.extract_menu:
 		application.register_popup_menu_action(archiver.extract_mimes, application.menu_manager.create_menu_item(item))
+	for item in archiver.create_menu:
+		application.register_popup_menu_action(archiver.create_mimes, application.menu_manager.create_menu_item(item))
 
